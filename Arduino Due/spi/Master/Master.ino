ea3eb221e9bfe6553d0a6635e97a71c2e86552f3
Maaz Sansare <maaz.sansare@phi-robotics.com>
2022-06-10 22:42:33 +0530

    Create Master.ino
    Arduino Due spi master

---
+++ Arduino Due/spi/Master/Master.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+
+void setup() {
+
+  pinMode(SS, OUTPUT);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+  Serial.begin(9600);
+}
+
+byte var = 4;
+byte chartosend[] = "Hello Slave";
+byte rec[sizeof(chartosend)];
+
+void loop (void) {
+   digitalWrite(SS, LOW); // enable Slave Select
+   // send test string
+   SPI.transfer (var);
+   for (int i = 0 ; i < sizeof(chartosend) ; i++) {
+      rec[i] = SPI.transfer (chartosend[i]);
+   }
+   digitalWrite(SS, HIGH); // disable Slave Select
+   Serial.println("\n new data \n");
+   for (int i = 0 ; i < 11 ; i++) {
+      Serial.println(char(rec[i]));
+   }
+   delay(5000);
+}
